42bb2d50df1b4fe91f244ba44887d0015b41b578
katsiaryna shupikava <katsiaryna.shupikava@mail.ru>
2024-07-18 15:29:21 +0300

    Initial commit

--- src/app/lesson-details/lesson-details.component.ts
+++ src/app/lesson-details/lesson-details.component.ts
@@ -2,13 +2,14 @@ import { Component, inject} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LessonsService } from '../lessons.service';
 import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Lesson } from '../lesson';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-lesson-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lesson-details.component.html',
   styleUrl: './lesson-details.component.css'
 })
@@ -24,13 +25,18 @@ export class LessonDetailsComponent {
   lessonLink: any = '';
   lessonDescription: any = '';
   lesson: Lesson | undefined;
+  isLinkAvailable = true;
 
   constructor() {
     this.lessonTitle = String(this.route.snapshot.params['title']);
 
     this.lesson = this.lessonService.getLessonByTitle(String(this.lessonTitle));
     if (this.lesson) {
-      this.lessonLink = this.sanitizer.bypassSecurityTrustResourceUrl(this.lesson.link);
+      if (this.lesson.link === '-') {
+        this.isLinkAvailable = false;
+      } else {
+        this.lessonLink = this.sanitizer.bypassSecurityTrustResourceUrl(this.lesson.link);
+      }
       let lessonDescriptionWithoutLinks = this.lesson.description.replace(/@/g, '<br>');
       let lessonDescriptionWithLinksAndStrong = lessonDescriptionWithoutLinks.replace(/!(.*?)!/g, '<strong>$1</strong>');
       this.lessonDescription = this.sanitizer.bypassSecurityTrustHtml(lessonDescriptionWithLinksAndStrong.replace(/\*(.*?)\*/g, '<a href="$1" target="_blank" style="color: rgb(83, 156, 132);"> (Click to see additional training material) </a>'));
